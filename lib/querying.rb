da092a232253ab43726679042f5c0a517ba592f4
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 17:27:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,9 @@ def select_value_and_count_of_most_prolific_species
   "SELECT name, COUNT(*)
   FROM characters
   HAVING MAX(COUNT(*))
-  GROUP BY name"
+  GROUP BY name
+  ORDER BY COUNT(*)
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
