f9a55a1aa7b57d5ad4eb1c663a59d9b14bf5dd4e
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 17:11:43 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id 
 
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);  
 
-CREATE TABLE characters_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);  
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);  
 
+INSERT INTO characters_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
