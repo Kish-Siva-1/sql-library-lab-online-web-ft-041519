e006e30d2e553c0228d7a778c980d9b01a13f7aa
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 17:09:37 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,4 +8,5 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id 
 
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);  
 
-CREATE TABLE characters_books (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);  
+CREATE TABLE characters_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);  
+
